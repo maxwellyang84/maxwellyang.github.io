3b0e066ad52fe0432516edd1100873d0e10253d9
Maxwell Yang <maxwellyang84@gmail.com>
2019-01-27 21:47:37 -0500

    changed documents

--- js/myScript.js
+++ js/myScript.js
@@ -1,78 +1,36 @@
 var btnMenu = document.getElementById("btn-menu");
+
 btnMenu.addEventListener("click", toggleMenu);
 
-
-function toggleMenu(e) {
-    
-    var divOverlay = document.getElementsByClassName("overlay")[0];
-    var speed = 10;
-    
-    
-    var eventTarget = e.target;
-    
-    if( eventTarget.className == "btn-open" ) {
+function toggleMenu(event) {
+    var divOverlay= document.getElementsByClassName("overlay")[0];
+var speed = 10;
+    var eventTarget = event.target; //returns element that triggered event
+    if(eventTarget.className == "btn-open"){
         fadeIn(divOverlay, speed);
         eventTarget.className = "btn-close";
-        
-    } else if ( eventTarget.className == "btn-close") {
+    }else if(eventTarget.className=="btn-close"){
         fadeOut(divOverlay, speed);
         eventTarget.className = "btn-open";
     }
-    
 }
 
-function fadeIn(elem, speed) {
-    
+function fadeIn(elem, speed){
     var inInterval = setInterval(function(){
-        
-    elem.style.opacity = Number(elem.style.opacity) + 0.02;
-    
-    if (elem.style.opacity >= 1) {
-        
-        elem.style.opacity = 1;
-        clearInterval(inInterval);
-        
-    }    
-        
-        
-    }, speed); // 10ms == .01s
-    
-    
-    
+        elem.style.opacity = Number(elem.style.opacity) + 0.02;
+        if(elem.style.opacity >=1){
+            elem.style.opacity = 1;
+            clearInterval(inInterval);
+        }
+    }, speed);
 }
 
-function fadeOut(elem, speed) {
-    
+function fadeOut(elem, speed){
     var outInterval = setInterval(function(){
-        
-    elem.style.opacity = Number(elem.style.opacity) - 0.02;
-    
-    if (elem.style.opacity <= 0) {
-        
-        elem.style.opacity = 0;
-        clearInterval(outInterval);
-        
-    }    
-        
-        
-    }, speed); // 10ms == .01s
-    
-    
-    
+        elem.style.opacity = Number(elem.style.opacity) - 0.02;
+        if(elem.style.opacity <=0){
+            elem.style.opacity = 0;
+            clearInterval(outInterval);
+        }
+    }, speed);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
